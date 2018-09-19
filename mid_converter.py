7facf3412e5629cc085a2fb36dd7c5706f5d0e74
oga <rgrm1916@gmail.com>
2018-09-20 01:37:39 +0900

    mid_converter.pyの完成

--- mid_converter.py
+++ mid_converter.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from scipy.special import comb
+import itertools
+
 input_file="sample.txt"
 output_file="mid_file"
 
@@ -22,4 +24,18 @@ with open(input_file, mode='r') as f:
             color=len(path)
         P.append(path)
 
-dimacs="p cnf {} {}".format(color*len(S), )
+dimacs="p cnf {} {}\n".format(color*len(S), (int(1+comb(len(S), 2))+len(P))*color)
+#one-hot constraint
+for i in range(color):
+    line=[i+1+j*color for j in range(len(S))]
+    dimacs+=' '.join(map(str, line))+'\n'
+    for k, l in itertools.combinations(line, 2):
+        dimacs+="{} {}\n".format(-k, -l)
+
+#output constraint
+for i in range(len(P)):
+    for j in range(color):
+        for k in range(len(P[i])):
+            dimacs+="{} ".format((int(P[i][k].replace('s', ''))-1)*color+j+1)
+        dimacs+='\n'
+print(dimacs)
